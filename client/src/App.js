0f8fff94b3741e8f42d1eb16feba31c31519be44
shruthi093 <jayasruthi.l@gmail.com>
2023-03-26 19:52:47 +0530

    v2.0

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import ContactManagerAPI from "./ContactManagerAPI"
+function App(){
+    return(
+        <ContactManagerAPI></ContactManagerAPI>
+    )
+}
+export default App
---
+++ server/models/ContactManager.js
@@ -0,0 +1,21 @@
+const mongoose=require("mongoose")
+/*mongoose.connect("mongodb://localhost:27017/BIDBSEP").then((res)=>console.log("connected to db successfully")).catch((err)=>console.log("connection failed",err))*/
+mongoose.connect(process.env.MONGO_CONNECTION_URL).then((res)=>console.log("connected to db successfully")).catch((err)=>console.log("connection failed",err))
+
+const ContactManager=mongoose.model("ContactManager",{
+   fname:{
+    type:String,
+    required:true,
+    unique:true
+   },
+   phone:{
+    type:Number,
+    required:true,
+   },
+   email:{
+    type:String,
+    required:true,
+    unique:true
+   }
+})
+module.exports=ContactManager
